5f874f64920d2e7439357cb7b5b093200bc15659
chinatsu <lolexplode@gmail.com>
2014-07-14 19:56:23 +0000

    maybe all of this is silly

--- modules/webload.py
+++ modules/webload.py
@@ -2,54 +2,89 @@
 """
 downloads module from web to willie/modules/ and loads it
 """
-import willie.module, requests, os, imp, json
+import willie.module, requests, os, imp, json, types
 from os.path import expanduser
 
 def setup(bot):
     global modulelist
     modulelist = requests.get("https://raw.githubusercontent.com/teamsrg/willie-modules/master/conf-module/webload.json").json()
+    global williedir
+    williedir = os.path.dirname(os.path.realpath(__file__ + "/../"))
+    print williedir
+
+
+def getdepends(depend, format_url):
+    abort_mission = 0
+    dependresp = requests.get(format_url + depend, stream=True)
+    if dependresp.status_code is not requests.codes.ok:
+        bot.reply("Respondse code for '{0}' not OK: {1}".format(dependresp, dependresp.status_code))
+        abort_mission = 1
+        return
+    if abort_mission:
+        return
+    with open("{0}/{1}".format(williedir, depend), "wb") as fd:
+        for chunk in dependresp.iter_content(1024):
+            if not chunk:
+                break
+            fd.write(chunk)
+
+    
+def sync(bot, module_name):
+    global williedir
+    global modulelist
+    if not module_name or module_name == bot.config.owner:
+        return bot.reply("Literally What?")
+    format_url = "https://raw.githubusercontent.com/teamsrg/willie-modules/master/"
+    resp = requests.get(format_url + "modules/" + module_name + ".py", stream=True)
+    if resp.status_code is not requests.codes.ok:
+        bot.reply("Response code for '{0}' not OK: {1}".format(module_name, resp.status_code))
+        return
+    with open("{0}/modules/{1}.py".format(williedir, module_name), "wb") as fh:
+        for chunk in resp.iter_content(1024):
+            if not chunk:
+                break
+            fh.write(chunk)
+    if modulelist["modulelist"][module_name]["depends"]:
+        depends = modulelist["modulelist"][module_name]["depends"]
+        if isinstance(depends, basestring):
+            getdepends(depends, format_url)
+        else:
+            for depen in depends:
+                getdepends(depen, format_url)
+    module = imp.load_source(module_name, "{0}/modules/{1}.py".format(williedir, module_name))
+    if hasattr(module, "setup"):
+        module.setup(bot)
+    bot.register(vars(module))
+    bot.bind_commands()
+    bot.reply(module)
+
 
 @willie.module.nickname_commands("webload")
 @willie.module.priority("low")
-@willie.module.example("webload list, webload sync")
+@willie.module.example("webload list, webload sync, webload update")
 @willie.module.thread(True)
 def webload(bot, trigger):
     """Does the same thing as 'reload.load' except loads the module from the repository"""
-    module_dir = os.path.dirname(os.path.realpath(__file__))
+    global williedir
     global modulelist
     if not trigger.admin:
         return
-    elif trigger.group(3) == "list":
+    if trigger.group(3) == "list":
         bot.say("Available modules:")
         for mod, val in modulelist["modulelist"].items():
-            if os.path.isfile(module_dir + "/" + mod + ".py"):
+            if os.path.isfile(williedir + "/modules/" + mod + ".py"):
                 comment = val["comment"] + " \x03[installed]"
             else:
                 comment = val["comment"]
             bot.say("[\x033" + val["state"] + "\x03] \x037" + mod + ": \x0312"  + comment)
     elif trigger.group(3) == "sync":
-        module_name = trigger.group(4)
-        if not module_name or module_name == bot.config.owner:
-            return bot.reply("Literally What?")
-        format_url = "https://raw.githubusercontent.com/teamsrg/willie-modules/master/modules/{0}.py"
-        with open("{0}/{1}.py".format(module_dir, module_name), "wb") as fh:
-            resp = requests.get(format_url.format(module_name), stream=True)
-            if resp.status_code is not requests.codes.ok:
-                bot.reply("Response code for '{0}' not OK: {1}".format(module_name, resp.status_code))
-                return
-            for chunk in resp.iter_content(1024):
-                if not chunk:
-                    break
-                fh.write(chunk)
-        module = imp.load_source(module_name, "{0}/{1}.py".format(module_dir, module_name))
-        if hasattr(module, "setup"):
-            module.setup(bot)
-        bot.register(vars(module))
-        bot.bind_commands()
-
-        bot.reply(module)
+        sync(bot, trigger.group(4))
+    elif trigger.group(3) == "update":
+        for mod, val in modulelist["modulelist"].items():
+            if os.path.isfile(williedir + "/modules/" + mod + ".py"):
+                sync(bot, mod)
     elif not trigger.group(3):
-        bot.say("Usage: !weblist list, !weblist sync module")
+        bot.say("Usage: !weblist list, !weblist sync module, !weblist update")
 
 # same for pms
 @willie.module.commands("webload")
