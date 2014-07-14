74e035520e59762d266dc22e20665a8bc60d8b19
xtrv <xtrv@users.noreply.github.com>
2014-07-14 23:28:08 +0300

    hue

--- modules/esa_sched.py
+++ modules/esa_sched.py
@@ -10,96 +10,106 @@ def setup(bot):
     with open("%s/.willie/conf-module/esashort.tsv" % expanduser("~"), "r") as f:
         esa_short = list(f)
     # header
-    # split
-    esa_hdr_long = esa_long[0].split("\t")
-    esa_hdr_short = esa_short[0].split("\t")
-    # strip
-    hdr = {}
-    for i in range(len(esa_hdr_long)):
-        if esa_hdr_long[i] and esa_hdr_long[i] != "\n":
-            hdr[i] = esa_hdr_long[i]
-    esa_hdr_long = hdr
-    hdr = {}
-    for i in range(len(esa_hdr_short)):
-        if esa_hdr_short[i] and esa_hdr_short[i] != "\n":
-            hdr[i] = esa_hdr_short[i]
-    esa_hdr_short = hdr
+    esa_hdr_long = parse_tsv_header(esa_long[0])
+    esa_hdr_short = parse_tsv_header(esa_short[0])
     # data
+    esa_long = parse_tsv_data(esa_hdr_long, esa_long)
+    esa_short = parse_tsv_data(esa_hdr_short, esa_short)
+
+
+def parse_tsv_header(header_line):
+    header_line = header_line.split("\t")
+    hdr = {}
+    for pos in range(len(header_line)):
+        if header_line[pos] and header_line[pos] != "\n":
+            hdr[header_line[pos].lower()] = pos
+    return hdr
+
+
+def parse_tsv_data(header, data_list):
     data = []
-    for i in range(1, len(esa_long)):
-        line = esa_long[i].split("\t")
+    for list_pos in range(len(data_list)):
+        current_list = data_list[list_pos].split("\t")
         data.append({})
-        for linei in range(len(line)):
-            if linei in esa_hdr_long:
-                data[i - 1][linei] = line[linei]
-    esa_long = data
-    data = []
-    for i in range(1, len(esa_short)):
-        line = esa_short[i].split("\t")
-        data.append({})
-        for linei in range(len(line)):
-            if linei in esa_hdr_long:
-                data[i - 1][linei] = line[linei]
-    esa_short = data
+        for line_pos in range(len(current_list)):
+            if line_pos in header.values():
+                data[list_pos - 1][line_pos] = current_list[line_pos]
+    return data
 
-
-def get_hdr_key(name, which="long"):
-    global esa_hdr_long, esa_hdr_short
-    name = name.lower()
-    hdr = esa_hdr_long if which == "long" else esa_short
-    for key in hdr.keys():
-        val = hdr[key].lower()
-        if name in val:
-            return key
-    return -1
-
+# header long
+#     {'category': 3, 'console': 5, 'runner': 7, 'region': 6, 'game': 2, 'date': 1, 'estimate': 4}
+# header short
+#     {'category': 3, 'console': 5, 'runner\n': 7, 'region': 6, 'game': 2, 'date': 1, 'estimate': 4}
+# esa long
+#     {1: '27-7-2014 15:10:00', 2: 'Ape Escape 3', 3: 'Any%', 4: '1:45:00', 5: 'PS2', 6: '', 7: 'Cerberus'}
+# esa_short
+#     {1: '28-7-2014 12:00:00', 2: 'World of Illusion', 3: 'Any% Co-op', 4: '0:30:00', 5: 'Genesis', 6: 'NTSC', 7: 'Edenal & Grukk\n'}
 
 def find_run(query, group="game", which="long"):
-    global data
+    global esa_long, esa_short, esa_hdr_long, esa_hdr_short
     query = query.lower()
-    hdr_id = get_hdr_key("thing" if which == "short" and group == "game" else group, which)
-    data = esa_long if which == "long" else esa_short
-    for run in data:
-        val = run[hdr_id].lower()
-        if query in val:
+    hdr_val = esa_hdr_long[group] if which == "long" else esa_hdr_short[group]
+    for run in esa_long if which == "long" else esa_short:
+        if run and query in run[hdr_val].lower():
             return run
+    return None
 
 
-def format_run(run):
-    # todo store all the keys instead of looking them up each time
-    formatted = "\x0309" + run[get_hdr_key("game")] + "\x03"
-    if run[get_hdr_key("category")] or run[get_hdr_key("region")]:
+def format_run(run, which="long"):
+    global esa_hdr_long, esa_hdr_short
+    hdr = esa_hdr_long if which == "long" else esa_hdr_short
+    formatted = "\x0309" + run[hdr["game"]] + "\x03"
+    category = run[hdr["category"]]
+    region = run[hdr["region"]]
+    runner = run[hdr["runner"]]
+    if category or region:
         formatted += " (\x0303"
-        if run[get_hdr_key("category")]:
-            formatted += run[get_hdr_key("category")]
-        if run[get_hdr_key("category")] and run[get_hdr_key("region")]:
+        if category:
+            formatted += category
+        if category and region:
             formatted += " - "
-        if run[get_hdr_key("region")]:
-            formatted += run[get_hdr_key("region")]
+        if region:
+            formatted += region
         formatted += "\x03)"
-    if run[get_hdr_key("runner")]:
-        formatted += " by \x0312" + run[get_hdr_key("runner")] + "\x03"
+    if runner:
+        formatted += " by \x0312" + runner + "\x03"
     return formatted
 
 
-@willie.module.commands("esa")
-@willie.module.example("esa cur(rent), esa prev(ious), esa next, esa game_name, esa long|short game_name")
-@willie.module.rate(10)
-def esa(bot, trigger):
-    if trigger.group(3) == "cur" or trigger.group(3) == "current":
-        pass
-    elif trigger.group(3) == "prev" or trigger.group(3) == "previous":
-        pass
-    elif trigger.group(3) == "next":
-        pass
-    elif trigger.group(3) == "long" or trigger.group(3) == "short":
-        pass
+@willie.module.commands("find")
+@willie.module.example("find [game|runner] keyword, first parameter is game by default")
+def cmd_find(bot, trigger):
+    if not trigger.group(3):
+        bot.reply("Literally What?")
+        return
+    group = trigger.group()
+    if trigger.group(3) == "runner" and trigger.group(4):
+        query = group[group.index(trigger.group(4)):]
+        run = find_run(query, "runner")
+        if run is None:
+            return
+        bot.reply(format_run(run))
     else:
-        query = ""
-        try:# are you FUCKING IOGONADOGIN kdidding me
-            for i in range(4, 50):
-                query += trigger.group(i) + " "
-        except:
-            pass
-        query = query.strip()
-        bot.reply(format_run(find_run(query)))
+        if trigger.group(3) == "game" and trigger.group(4):
+            query = group[group.index(trigger.group(4)):]
+        else:
+            query = group[group.index(trigger.group(3)):]
+        run = find_run(query)
+        if run is None:
+            return
+        bot.reply(format_run(run))
+
+
+@willie.module.commands("cur(rent|ry)?")
+def cmd_current(bot, trigger):
+    bot.say("oiii")
+
+
+@willie.module.commands("next")
+def cmd_next(bot, trigger):
+    bot.say("ayyy")
+
+
+@willie.module.commands("prev(ious)?")
+def cmd_previous(bot, trigger):
+    bot.say("lmao")
