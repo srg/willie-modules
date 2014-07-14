51dbffbc70e374c956bd862b50a851c613521c96
xtrv <xtrv@users.noreply.github.com>
2014-07-15 02:28:16 +0300

    esa schedule is almost working\nsome problems with the current game though

--- modules/esa_sched.py
+++ modules/esa_sched.py
@@ -1,5 +1,11 @@
+"""
+pip install python-dateutil pytz
+"""
 import willie.module
 from os.path import expanduser
+from pytz import timezone
+from datetime import datetime
+from dateutil import parser
 
 
 def setup(bot):
@@ -13,8 +19,16 @@ def setup(bot):
     esa_hdr_long = parse_tsv_header(esa_long[0])
     esa_hdr_short = parse_tsv_header(esa_short[0])
     # data
-    esa_long = parse_tsv_data(esa_hdr_long, esa_long)
-    esa_short = parse_tsv_data(esa_hdr_short, esa_short)
+    esa_long = dates_to_timestamps(esa_hdr_long, parse_tsv_data(esa_hdr_long, esa_long))
+    esa_short = dates_to_timestamps(esa_hdr_short, parse_tsv_data(esa_hdr_short, esa_short))
+
+
+def dates_to_timestamps(header, runs):
+    date_key = header["date"]
+    for pos in range(len(runs)):
+        if runs[pos]:
+            runs[pos][date_key] = int(parser.parse(runs[pos][date_key]).strftime("%s"))
+    return runs
 
 
 def parse_tsv_header(header_line):
@@ -36,14 +50,6 @@ def parse_tsv_data(header, data_list):
                 data[list_pos - 1][line_pos] = current_list[line_pos]
     return data
 
-# header long
-#     {'category': 3, 'console': 5, 'runner': 7, 'region': 6, 'game': 2, 'date': 1, 'estimate': 4}
-# header short
-#     {'category': 3, 'console': 5, 'runner\n': 7, 'region': 6, 'game': 2, 'date': 1, 'estimate': 4}
-# esa long
-#     {1: '27-7-2014 15:10:00', 2: 'Ape Escape 3', 3: 'Any%', 4: '1:45:00', 5: 'PS2', 6: '', 7: 'Cerberus'}
-# esa_short
-#     {1: '28-7-2014 12:00:00', 2: 'World of Illusion', 3: 'Any% Co-op', 4: '0:30:00', 5: 'Genesis', 6: 'NTSC', 7: 'Edenal & Grukk\n'}
 
 def find_run(query, group="game", which="long"):
     global esa_long, esa_short, esa_hdr_long, esa_hdr_short
@@ -52,6 +58,30 @@ def find_run(query, group="game", which="long"):
     for run in esa_long if which == "long" else esa_short:
         if run and query in run[hdr_val].lower():
             return run
+    return None
+
+
+def find_run_date(trigger, target="current"):
+    global esa_long, esa_short, esa_hdr_long, esa_hdr_short
+    group = trigger.group()
+    runs = esa_long
+    hdr_key = esa_hdr_long["date"]
+    if " " in group and group[group.index(" ") + 1:] == "short":
+        runs = esa_short
+        hdr_key = esa_hdr_short["date"] # sure the header keys are the same for both but eh
+    time_now = int(datetime.now(timezone("Europe/Stockholm")).strftime("%s"))
+    for pos in range(len(runs)):
+        if runs[pos][hdr_key] >= time_now:
+            if target == "current":
+                return runs[pos]
+            elif target == "next":
+                if pos == len(runs):
+                    return None
+                return runs[pos + 1]
+            else: # previous
+                if pos == 0:
+                    return None
+                return runs[pos - 1]
     return None
 
 
@@ -76,9 +106,32 @@ def format_run(run, which="long"):
     return formatted
 
 
+def format_next_run(run, which="long"):
+    formatted = format_run(run, which)
+    time_now = int(datetime.now(timezone("Europe/Stockholm")).strftime("%s"))
+    time_then = run[esa_hdr_long["date"]] if which == "long" else run[esa_hdr_short["date"]]
+    seconds = time_then - time_now
+    minutes = seconds / 60
+    hours = minutes / 60
+    days = hours / 24
+    time = ""
+    if days > 0:
+        time += repr(days) + " days, "
+    if hours > 0:
+        time += repr(hours % 24) + " hours, "
+    if minutes > 0:
+        time += repr(minutes % 60) + " mins, "
+    time = time[:-2]
+    formatted += " in about \x0308" + time + "\x03"
+    return formatted
+
+
 @willie.module.commands("find")
-@willie.module.example("find [game|runner] keyword, first parameter is game by default")
+@willie.module.example("find [game|runner] keyword, first parameter is optional and is game by default")
 def cmd_find(bot, trigger):
+    """
+    Returns the first match for keyword of type game or runner in the ESA schedule
+    """
     if not trigger.group(3):
         bot.reply("Literally What?")
         return
@@ -100,16 +153,37 @@ def cmd_find(bot, trigger):
         bot.reply(format_run(run))
 
 
-@willie.module.commands("cur(rent|ry)?")
+@willie.module.commands("cur")
 def cmd_current(bot, trigger):
-    bot.say("oiii")
+    """
+    Returns approximately the current run in the ESA schedule
+    """
+    run = find_run_date(trigger)
+    if run:
+        bot.say(format_run(run))
+    else:
+        bot.reply("Is the event even live yet? :V")
 
 
 @willie.module.commands("next")
 def cmd_next(bot, trigger):
-    bot.say("ayyy")
+    """
+    Returns approximately the next run in the ESA schedule
+    """
+    run = find_run_date(trigger, "next")
+    if run:
+        bot.say(format_next_run(run))
+    else:
+        bot.reply("This is the last run!")
 
 
-@willie.module.commands("prev(ious)?")
+@willie.module.commands("prev")
 def cmd_previous(bot, trigger):
-    bot.say("lmao")
+    """
+    Returns approximately the previous run in the ESA schedule
+    """
+    run = find_run_date(trigger, "previous")
+    if run:
+        bot.say(format_run(run))
+    else:
+        bot.reply("This is the first run!")
