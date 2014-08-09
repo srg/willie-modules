d3458e0590a0d8a56d74217072ec2d03580c7755
chinatsu <lolexplode@gmail.com>
2014-08-09 09:50:11 +0000

    change to lurk's hitbox team, general maintenance of streams.jsony

--- modules/streams.py
+++ modules/streams.py
@@ -21,8 +21,14 @@ def streams(bot,trigger):
         streamlist = ''.join(streamers) # make a list of streamers, separated with ',' for our twitch api request
     tw = requests.get('https://api.twitch.tv/kraken/streams?channel=' + streamlist)
     twjson = tw.json()
-    hb = requests.get('https://www.hitbox.tv/api/team/speedrunninggeneral?filter=&limit=&liveonly=true&media=true') # hitbox srg team support
+    hb = requests.get('https://www.hitbox.tv/api/team/speedfriends?filter=&limit=&liveonly=true&media=true') # hitbox srg team support
     hbjson = hb.json()
+    for hbstream in hbjson['media']['livestream']: # hitbox.tv \o/
+        counter += 1
+        online.append("\x037http://hitbox.tv/" + hbstream['media_user_name'] + "\x03 " + hbstream['media_status'] + " \x033|\x03 ")
+        if counter % 4 == 0:
+            linelist.append(online)
+            online = []
     for stream in twjson['streams']:
         if not stream['game']:
             descrip = stream['channel']['status'].rstrip('\n')
@@ -33,12 +39,6 @@ def streams(bot,trigger):
         online.append("\x037http://twitch.tv/" + stream['channel']['name'] + " \x03" + descrip + " \x033|\x03 ")
         counter += 1
         if counter % 4 == 0: # have a maximum of 4 streamers per line, the low-ish number is because link + stream titles can get rather long.
-            linelist.append(online)
-            online = []
-    for hbstream in hbjson['media']['livestream']: # hitbox.tv \o/
-        counter += 1
-        online.append("\x037http://hitbox.tv/" + hbstream['media_user_name'] + "\x03 " + hbstream['media_status'] + " \x033|\x03 ")
-        if counter % 4 == 0:
             linelist.append(online)
             online = []
     if online: # get the remaining streamers on the last line
