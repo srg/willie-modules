e8f4313a67fb0f3998d1ad6193195feef2b0a4eb
xtrv <xtrv@users.noreply.github.com>
2014-08-11 15:48:40 +0300

    streeemerz

--- modules/streams.py
+++ modules/streams.py
@@ -1,4 +1,4 @@
-import willie, sys, requests, json
+import willie, sys, json
 from os.path import expanduser
 
 def setup(bot):
@@ -7,48 +7,62 @@ def setup(bot):
     twitchlist = json.load(f)
     f.close()
 
-@willie.module.commands('streams')
-@willie.module.thread(True)
-def streams(bot,trigger):
-    """Sends the channel a list of currently live streams in a list"""
+def lookup_twitch():
     global twitchlist
-    streamers = []
-    online = []
-    linelist = []
-    counter = 0
-    for streamer in twitchlist["streamlist"]:
-        streamers.append(streamer + ",")
-        streamlist = ''.join(streamers) # make a list of streamers, separated with ',' for our twitch api request
+    twitchers = []
+    for twitcher in twitchlist["streamlist"]:
+        twitchers.append(twitcher)
+    twitchers = ",".join(twitchers)
     try:
-        tw = requests.get('https://api.twitch.tv/kraken/streams?channel=' + streamlist)
-    except requests.ConnectionError:
-        bot.say("something went wrong w/ twitch")
-    twjson = tw.json()
-    hb = requests.get('https://www.hitbox.tv/api/team/speedfriends?filter=&limit=&liveonly=true&media=true') # hitbox srg team support
-    hbjson = hb.json()
-    for hbstream in hbjson['media']['livestream']: # hitbox.tv \o/
-        counter += 1
-        online.append("\x0313http://hitbox.tv/" + hbstream['media_user_name'] + "\x03 " + hbstream['media_status'] + " \x0313|\x03 ")
-        if counter % 4 == 0:
-            linelist.append(online)
-            online = []
-    for stream in twjson['streams']:
-        if not stream['game']:
-            descrip = stream['channel']['status'].rstrip('\n')
-        elif len(stream['channel']['status']) < 2: # if the stream title is too short, get the game title instead
-            descrip = stream['game']
-        else:
-             descrip = stream['channel']['status'].rstrip('\n')
-        online.append("\x037http://twitch.tv/" + stream['channel']['name'] + " \x03" + descrip + " \x033|\x03 ")
-        counter += 1
-        if counter % 4 == 0: # have a maximum of 4 streamers per line, the low-ish number is because link + stream titles can get rather long.
-            linelist.append(online)
-            online = []
-    if online: # get the remaining streamers on the last line
-        linelist.append(online)
-        online = []
-    if linelist:
-        for line in linelist:
-            bot.say(''.join(line)[:-3]) # cut the last 3 characters which are normally used as dividers
-    else:
-        bot.say(trigger.nick + ": No streams found, go check \x037http://www.twitch.tv/directory/following\x03")
+        js = willie.web.get("https://api.twitch.tv/kraken/streams?channel=%s" % twitchers, timeout=10, verify_ssl=False)
+        return json.loads(js)
+    except:# whatever web.get throws and possible valueerror from json.loads
+        return None
+
+def lookup_hitbox(team="speedfriends"):
+    try:
+        js = willie.web.get("https://api.hitbox.tv/team/%s?liveonly=true&media=true" % team, timeout=10, verify_ssl=False)
+        return json.loads(js)
+    except:# same thing here
+        return None
+
+@willie.module.commands("streams")
+@willie.module.thread(True)
+def streams(bot, trigger):
+    tw = lookup_twitch()
+    hb = lookup_hitbox()
+
+    msgs = []
+    msg = ""
+    c = 0
+
+    if hb is not None:
+        streams = hb["media"]["livestream"]
+        for stream in streams:
+            msg += "\x0313https://hitbox.tv/%s\x03 %s \x0313|\x03 " % (stream["media_user_name"], stream["media_status"])
+            c += 1
+            if c % 4 is 0:
+                msgs.append(msg)
+                msg = ""
+
+    if tw is not None:
+        streams = tw["streams"]
+        for stream in streams:
+            if len(stream["channel"]["status"]) < 2 and stream["game"]:
+                description = stream["game"]
+            else:
+                description = stream["channel"]["status"].rstrip("\n")
+            msg += "\x037http://twitch.tv/%s\x03 %s \x033|\x03 " % (stream["channel"]["name"], description)
+            c += 1
+            if c % 4 is 0:
+                msgs.append(msg)
+                msg = ""
+
+    if msg:# add the remaining streams
+        msgs.append(msg)
+
+    if not msgs:
+        bot.reply("No streams found, try again later")
+        return
+    for msg in msgs:
+        bot.say(msg[:-3])# cut the last 3 characters which are normally used as dividers
